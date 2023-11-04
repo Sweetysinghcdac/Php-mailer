cdea206647ab74898da7b145f7b090d16e1eb95f
Sweetysinghcdac <sweetysinghcdac@gmail.com>
2023-11-04 13:04:01 +0530

    Mail has been successfully send using phpmailer() to single user

--- smtp_phpmailer.php
+++ smtp_phpmailer.php
@@ -3,39 +3,39 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 use PHPMailer\PHPMailer\SMTP;
 
-require 'path/to/PHPMailer/src/Exception.php';
-require 'path/to/PHPMailer/src/PHPMailer.php';
-require 'path/to/PHPMailer/src/SMTP.php';
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
 
 $mail = new PHPMailer(true);
 
 try {
     //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'sweetysinghcdac@gmail.com';                     //SMTP username
-    $mail->Password   = 'refx qsvj wxiz xvay';                               //SMTP password
+    $mail->Password   = 'refxqsvjwxizxvay';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     //Recipients
-    $mail->setFrom('sweetysinghcdac@gmail.com', 'Mailer');
-    $mail->addAddress('sweetysinghrajput8953@gmail.com', 'Joe User');     //Add a recipient
-    $mail->addAddress('ss0605180@gmail.com');               //Name is optional
-    $mail->addReplyTo('sweetysinghcdac@gmail.com', 'Information');
+    $mail->setFrom('sweetysinghcdac@gmail.com','DCS Team');
+    $mail->addAddress('sweetysinghrajput8953@gmail.com' );     //Add a recipient
+    // $mail->addAddress('ss0605180@gmail.com');               //Name is optional
+    // $mail->addReplyTo('sweetysinghcdac@gmail.com', 'Information');
     // $mail->addCC('cc@example.com');
     // $mail->addBCC('bcc@example.com');
 
     //Attachments
-    $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-    $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+    // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
 
     //Content
     $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = 'Here is the subject';
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->Subject = 'Ignore this mail';
+    $mail->Body    = 'Hi, <br> This is message form bulk mail <br> Just ignore this mail <b>in bold!</b>';
     $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
     $mail->send();
